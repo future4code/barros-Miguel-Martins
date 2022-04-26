4509696cdc5f154fb218ee8b26c41dd1f1761f8b
Miguel Ambrosio Martins <miguelmartinsambrosio@gmail.com>
2022-04-26 12:12:14 -0300

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,9 @@
+/* 1. a) vai aparecer {canal: 'Globo', horario: '14h'}
+2 a) vai aparecer {nome: 'Juca', idade: 3, raca: 'SRD'}
+VM85:12 {nome: 'Juba', idade: 3, raca: 'SRD'}
+VM85:13 {nome: 'Jubo', idade: 3, raca: 'SRD'}
+
+
+
+
+*/
