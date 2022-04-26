56034af986de5fa34356de5f90a56d116246ecdd
Miguel Ambrosio Martins <miguelmartinsambrosio@gmail.com>
2022-04-26 12:39:48 -0300

    questão 1 e doi incompleto

--- modulo1/variaveis/template_javascript/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -1,5 +1,29 @@
+// EXERCÍCIO DE INTERPRETAÇÃO 
+/*
+1. VAI APARECER O RESULTADO 10
+O RESULTADO SERÁ 10,5
+2. O RESULTADO SERÁ 10,20,10
+3
+*/
 let nome
 let idade
+console.log(typeof nome , idade)
+// será impreso indefinido, pois não foi colocador um valor 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(typeof nome)
 console.log(typeof idade)
 
